cc0ea91f6cdb8e84e19ed49ea9839426afee9291
Lim Pureum <dlavnfma22@naver.com>
2016-09-26 11:30:30 +0900

    fix auth module

--- meteor-angular/imports/ui/components/auth/auth.js
+++ meteor-angular/imports/ui/components/auth/auth.js
@@ -6,6 +6,7 @@ import { Accounts } from 'meteor/accounts-base';
 
 import template from './auth.html';
 import { name as Login } from '../login/login';
+import { name as DisplayNameFilter } from '../../filters/displayNameFilter';
 import { name as Register } from '../register/register';
 import { name as Password } from '../password/password';
 
@@ -37,6 +38,7 @@ export default angular.module(name, [
   angularMeteor,
   Login,
   Register,
+  DisplayNameFilter,
   Password
 ]).component(name, {
   template,

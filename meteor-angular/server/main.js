c713a842edb30cfde3b15214653e073d0b34419a
Lim Pureum <dlavnfma22@naver.com>
2016-09-26 16:59:40 +0900

    create the metemq server

--- meteor-angular/server/main.js
+++ meteor-angular/server/main.js
@@ -1,5 +1,8 @@
 import '../imports/startup/fixtures';
 import '../imports/api/users';
-import 'meteor/metemq:metemq'
 import '../imports/api/things';
 import '../imports/api/thingsInbox';
+
+import { Source } from 'meteor/metemq:metemq'
+
+new Source();
